b36026d31b247a46e3ef5b225da2fa57120f1a33
Christopher Johnson <johnsonce@goldmail.etsu.edu>
2021-10-11 20:06:56 -0400

    just do it

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -31,7 +31,7 @@ export default class MyApp extends React.Component {
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: process.env.REACT_APP_APP_ID,
+      sandbox: "AUEzn5eRxvR9Psd_20l4exfso_O508LNng3K6yEFRV9-L5lcOT5l8k1GI-TkpvJA0HNDv6XcHGT0qQuX",
       production: "YOUR-PRODUCTION-APP-ID"
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
--- src/index.js
+++ src/index.js
@@ -20,4 +20,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
+require('dotenv').config();
+
 reportWebVitals();
